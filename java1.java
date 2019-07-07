1c7b797690b45db9473b39fe5fc6d8b4f7010ef8
harpreetsingh786 <52627204+harpreetsingh786@users.noreply.github.com>
2019-07-07 09:53:13 -0400

    Update java1.java

--- java1.java
+++ java1.java
@@ -5,6 +5,11 @@ public static void main(String[] args)
 /*********
 write a program thast adds a and b and store in the c
 write A=3,B=4,C=5
+**********************/
+  int A=3;
+  int B=4;
+  int c=A+B;
+   
 
 
 }//end  of main
