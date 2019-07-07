07072f7bb8d98e0a19bcfca07e2a77ce2c9cd01a
harpreetsingh786 <52627204+harpreetsingh786@users.noreply.github.com>
2019-07-07 09:22:26 -0400

    Create java1.java

---
+++ java1.java
@@ -0,0 +1,15 @@
+public class Main
+{
+public static void main(String[] args)
+{
+/*********
+write a program thast adds a and b and store in the c
+write A=3,B=4,C=5
+
+
+}//end  of main
+
+
+
+
+}//end of class 
